9dcba0ddcb00c9ddd3119d2c1de631432f690389
Selena Mead <selenaelizabethmead@gmail.com>
2022-07-14 18:34:52 -0400

    many changes

--- Messenger1/controllers/ConversationsViewController.swift
+++ Messenger1/controllers/ConversationsViewController.swift
@@ -12,6 +12,30 @@ class ConversationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn{
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            let appearance = UINavigationBarAppearance()
+                appearance.configureWithOpaqueBackground()
+                UINavigationBar.appearance().standardAppearance = appearance
+                UINavigationBar.appearance().scrollEdgeAppearance = appearance
+          
+            
+            
+            nav.modalPresentationStyle = .fullScreen
+    
+            present(nav, animated: false)
+            
+            
+        }
     }
 
 
